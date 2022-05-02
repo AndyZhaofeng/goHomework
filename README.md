2ab7f995dd9020ec33f02644e68eee0493b262bf
赵峰 <526637166@qq.com>
2022-05-02 21:26:01 +0800

    docker启动

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goHomework
 极客时间云原生课程每周作业
+
+#模块三
+docker run -p 127.0.0.1:8080:8080 httpserver
---
+++ httpserver/src/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	userAgent := r.Header.Get("User-Agent")
+	javaHome := os.Getenv("JAVA_HOME")
+	ip := ReadUserIP(r)
+	w.Header().Set("RequestClient", userAgent)
+	w.Header().Set("javaHome", javaHome)
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "URL.Path = %q\n userAgent = %q\n javaHome = %q\n ip = %q\n",
+	r.URL.Path, userAgent, javaHome, ip)
+}
+
+func ReadUserIP(r *http.Request) string {
+    IPAddress := r.Header.Get("X-Real-Ip")
+    if IPAddress == "" {
+        IPAddress = r.Header.Get("X-Forwarded-For")
+    }
+    if IPAddress == "" {
+        IPAddress = r.RemoteAddr
+    }
+    return IPAddress
+}
